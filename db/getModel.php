44713d683833fdb161c69a2c6c53bc5c83999226
Martin Šebor <sebor.m@sosceskybrod.cz>
2024-05-13 14:13:39 +0200

    Added conn.php connection file, also getModel.php getter

---
+++ db/getModel.php
@@ -0,0 +1,20 @@
+<?php
+
+require "conn.php";
+
+function getCarInfo(){
+    $conn = $GLOBALS["conn"];
+
+    $carFullInfo = array();
+
+    $sql = "SELECT Model.url, Car.model, Color.name FROM Model JOIN Car ON Model.idCar=Car.id JOIN Color ON Color.id=Model.idColor";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute();
+    $stmt -> bind_result($url, $model, $color);
+    while ($stmt -> fetch())
+    {
+        array_push($carFullInfo, array("url" => $url, "model" => $model, "color" => $color));
+    }
+
+    return $carFullInfo;
+}
