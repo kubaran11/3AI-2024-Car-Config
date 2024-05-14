bf6e41920289c7e2a8ef12ff0cab5f39261180e6
Martin Šebor <sebor.m@sosceskybrod.cz>
2024-05-14 08:24:32 +0200

    Updated getModel.php to return more values

--- db/getModel.php
+++ db/getModel.php
@@ -7,13 +7,17 @@ function getCarInfo(){
 
     $carFullInfo = array();
 
-    $sql = "SELECT Model.url, Car.model, Color.name FROM Model JOIN Car ON Model.idCar=Car.id JOIN Color ON Color.id=Model.idColor";
+    $sql = "SELECT Model.url, Car.model, Color.name, Body.name, Addons.name FROM Model 
+            JOIN Car ON Model.idCar=Car.id 
+            JOIN Color ON Color.id=Model.idColor 
+            JOIN Body ON Body.id=Model.idBody 
+            JOIN Addons ON Addons.id=Model.idAddons";
     $stmt = $conn -> prepare ($sql);
     $stmt -> execute();
-    $stmt -> bind_result($url, $model, $color);
+    $stmt -> bind_result($url, $model, $color, $body, $addon);
     while ($stmt -> fetch())
     {
-        array_push($carFullInfo, array("url" => $url, "model" => $model, "color" => $color));
+        array_push($carFullInfo, array("url" => $url, "model" => $model, "color" => $color, "body" => $body, "addon" => $addon));
     }
 
     return $carFullInfo;
