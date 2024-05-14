57accb1fdf3e1378d5b18e1f95ed2252f5975a08
František Vrbata <vrbata.f@sosceskybrod.cz>
2024-05-14 09:56:11 +0200

    Added getCar.php

---
+++ db/getCar.php
@@ -0,0 +1,44 @@
+<?php
+
+require "conn.php";
+require "getCar.php"
+
+function getCarId(){
+    $conn = $GLOBALS["conn"];
+
+    $sql = "SELECT id FROM Car";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute();
+    $stmt -> bind_result($id);
+    $stmt -> fetch();
+
+    return $id;
+
+}
+function getCarModel(){
+    $conn = $GLOBALS["conn"];
+
+    $sql = "SELECT model FROM Car";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute();
+    $stmt -> bind_result($model);
+    $stmt -> fetch();
+   
+    return $model;
+
+}
+function getCarPrice(){
+    $conn = $GLOBALS["conn"];
+
+    $sql = "SELECT price FROM Car";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute();
+    $stmt -> bind_result($price);
+    $stmt -> fetch();
+   
+    return $price;
+
+}
+
+
+?>
