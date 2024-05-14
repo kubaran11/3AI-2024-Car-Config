ed461f1c567617b6a20c172925270283511aa1ff
Novakden <novakden2005@gmail.com>
2024-05-14 08:48:13 +0200

    Update getBody.php

--- db/getBody.php
+++ db/getBody.php
@@ -7,13 +7,13 @@ function getBody(){
 
     $body = array();
 
-    $sql = "SELECT name, price, idBody FROM Body";
+    $sql = "SELECT name, price, id FROM Body";
     $stmt = $conn -> prepare ($sql);
     $stmt -> execute();
-    $stmt -> bind_result($name, $price, $idBody);
+    $stmt -> bind_result($name, $price, $id);
     while ($stmt -> fetch())
     {
-        array_push($body, array("name" => $name, "price" => $price, "idBody" => $idBody));
+        array_push($body, array("name" => $name, "price" => $price, "id" => $id));
     }
 
     return $body;
