6fc5b334f94b78187151f834bd0b0f83016186ba
JRo10 <roubal.j@sosceskybrod.cz>
2024-05-14 14:26:20 +0200

    header html + css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webová Stránka</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="images/spectra.png" alt="Spectra" height="85" width="130">
+            </div>
+            <a>Models</a>
+        </div>
+    </header>
+    <div class="text">
+        <marquee id="gfg">
+             <img src="images/auto do leva.gif" alt="gif1">
+        </marquee>
+
+    </div>
+</body>
+</html>
