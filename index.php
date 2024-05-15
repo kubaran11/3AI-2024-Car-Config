2451f00dff23961a79436761281be17350c439af
JRo10 <roubal.j@sosceskybrod.cz>
2024-05-15 08:06:26 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
 </head>
 <body>
     <header>
-        <div class="container">
+        <div class="box">
             <div class="logo">
                 <img src="images/spectra.png" alt="Spectra" height="85" width="130">
             </div>
             <a>Models</a>
         </div>
     </header>
-    <div class="text">
-        <marquee id="gfg">
-             <img src="images/auto do leva.gif" alt="gif1">
+    <div class="auto">
+        <marquee>
+             <img src="images/auto.gif" alt="auto">
         </marquee>
 
     </div>
